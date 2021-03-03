de0e9a3e0258a1d99a0ca450d9225755c1faed88
Keith Fenske <github.kwfenske@gmail.com>
2021-03-03 10:15:21 -0700

    Add files via upload

--- CompareFolders3.java
+++ CompareFolders3.java
@@ -428,7 +428,7 @@ public class CompareFolders3
       word = args[i].toLowerCase(); // easier to process if consistent case
       if (word.length() == 0)
       {
-        /* Ignore null parameters, which are more common that you might think,
+        /* Ignore empty parameters, which are more common than you might think,
         when programs are being run from inside scripts (command files). */
       }
 
